5315138b37079610a5736d3eeb9bc3c1f339f401
Enzo Barbeli <enzobarbeli@gmail.com>
2026-04-28 21:37:31 -0300

    criação do AppBdContext

--- GameStoreMVC/Data/AppDbContext.cs
+++ GameStoreMVC/Data/AppDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace GameStoreMVC.Data
+﻿using GameStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        }
     }
 }
