1feaace3ba577baeccff91deb1962b22de5ad774
Enzo Barbeli <enzobarbeli@gmail.com>
2026-04-28 21:43:14 -0300

    Criação do UserRepository e do GameRepository

--- GameStoreMVC/Repositories/GameRepository.cs
+++ GameStoreMVC/Repositories/GameRepository.cs
@@ -1,9 +1,49 @@
-﻿using GameStoreMVC.Interfaces;
+﻿using GameStore.Data;
+using GameStore.Models;
+using GameStore.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace GameStoreMVC.Repositorio
+namespace GameStore.Repositories
 {
-    public class GameRepository: IGameRepositorio
+    public class GameRepository : IGameRepository
     {
-        
+        private readonly AppDbContext _context;
+
+        public GameRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Game>> GetAllAsync()
+        {
+            return await _context.Games.ToListAsync();
+        }
+
+        public async Task<Game?> GetByIdAsync(int id)
+        {
+            return await _context.Games.FindAsync(id);
+        }
+
+        public async Task AddAsync(Game game)
+        {
+            await _context.Games.AddAsync(game);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Game game)
+        {
+            _context.Games.Update(game);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var game = await GetByIdAsync(id);
+            if (game != null)
+            {
+                _context.Games.Remove(game);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
--- GameStoreMVC/Repositories/UserRepository.cs
+++ GameStoreMVC/Repositories/UserRepository.cs
@@ -1,9 +1,39 @@
-﻿using GameStoreMVC.Interfaces;
+﻿using GameStore.Data;
+using GameStore.Models;
+using GameStore.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace GameStoreMVC.Repositorio
+namespace GameStore.Repositories
 {
-    public class UserRepository: IUsuarioRepositorio
+    public class UserRepository : IUserRepository
     {
-      
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public User? GetByEmail(string email)
+        {
+            return _context.Users.FirstOrDefault(u => u.Email == email);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Add(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
     }
 }
