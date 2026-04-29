1442f3b717419841e0d8b557333448cc98fa1c6f
Enzo FB <103038972+EnzoFigueiredoBarbeli@users.noreply.github.com>
2026-04-29 00:09:22 -0300

    Correção de erro em IGameRepository.cs

---
+++ GameStoreMVC/Interfaces/IGameRepository.cs
@@ -0,0 +1,13 @@
+using GameStore.Models;
+
+namespace GameStore.Repositories.Interfaces
+{
+    public interface IGameRepository
+    {
+        Task<IEnumerable<Game>> GetAllAsync();
+        Task<Game?> GetByIdAsync(int id);
+        Task AddAsync(Game game);
+        Task UpdateAsync(Game game);
+        Task DeleteAsync(int id);
+    }
+}
